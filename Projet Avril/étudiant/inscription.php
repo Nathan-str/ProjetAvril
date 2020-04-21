a2678b5072e7e6c0460dfa54538fad337d7de2fd
Nathan Sestre <sestre.nathan@orange.fr>
2020-04-21 16:51:47 +0200

    Vérification doublon adresse mails et numéros

--- Projet Avril/étudiant/inscription.php
+++ Projet Avril/étudiant/inscription.php
@@ -1,23 +1,54 @@
 <?php
 	session_start();
 
-	$nom = $_POST['nom'];
+	$donne = fopen('fichiers/comptes.csv', 'r+');
 
-	//Vérifie si le formulaire a bien été rempli
-	if (isset($_POST['nom']) && isset($_POST['prenom']) && isset($_POST['mail']) && isset($_POST['mdp']) && isset($_POST['mdp1']) && !empty($_POST["nom"]) && !empty($_POST['prenom']) && !empty($_POST['mail']) && !empty($_POST['mdp']) && !empty($_POST['mdp1']) && $_POST['mdp'] == $_POST['mdp1']){
-		$donnes = fopen('fichiers/comptes.csv', 'a+');
+	while(!feof($donne)){
+		$ligne = fgets($donne);
+		if ($ligne != "") {
+			$lignes = substr($ligne, 0,-2);
+			$tableau = explode(";", $lignes);
 
-		//Met les éléments du formulaire dans le fichier et hache le mot de passe et définie un ID
-		fputs($donnes, $_POST['nom'] . ";" . $_POST["prenom"] . ";" . $_POST["mail"] . ";" . $_POST["numero"] . ";" . hash("sha256",$_POST['mdp']) . ";" . $_POST['filiere'] . ";" . $_POST['groupe'] . "\n");
+			if ($_POST['mail'] == $tableau[2]){
+				$sortie = false;
+				$AdresseMail = true;
+				break;
+			}else{
+				$sortie = true;
+				$AdresseMail = false;
+			}if ($_POST['numero'] == $tableau[3]) {
+				$sortie = false;
+				$numeroTelephone = true;
+				break;
+			}else{
+				$sortie = true;
+				$numeroTelephone = false;
+			}
+		}
+	}
 
-		fclose($donnes);
+	if ($sortie == true){
 
-		header("location:./redirection.php");
+		$nom = $_POST['nom'];
 
-	}elseif($_POST["mdp"] != $_POST["mdp1"]){
-		//Si il y a une erreur alors il est redirigé vers l'accueil
-		header("location:./redirection.php?error=3");
-	}elseif($_POST["mdp"]<5 || $_POST["mdp1"]<5){
-		header("location:./redirection.php?error=4");
+		//Vérifie si le formulaire a bien été rempli
+		if (isset($_POST['nom']) && isset($_POST['prenom']) && isset($_POST['mail']) && isset($_POST['mdp']) && isset($_POST['mdp1']) && !empty($_POST["nom"]) && !empty($_POST['prenom']) && !empty($_POST['mail']) && !empty($_POST['mdp']) && !empty($_POST['mdp1']) && $_POST['mdp'] == $_POST['mdp1']){
+			$donnes = fopen('fichiers/comptes.csv', 'a+');
+
+			//Met les éléments du formulaire dans le fichier et hache le mot de passe et définie un ID
+			fputs($donnes, $_POST['nom'] . ";" . $_POST["prenom"] . ";" . $_POST["mail"] . ";" . $_POST["numero"] . ";" . hash("sha256",$_POST['mdp']) . ";" . $_POST['filiere'] . ";" . $_POST['groupe'] . "\n");
+
+			fclose($donnes);
+
+			header("location:./redirection.php");
+
+		}elseif($_POST["mdp"] != $_POST["mdp1"]){
+			//Si il y a une erreur alors il est redirigé vers l'accueil
+			header("location:./redirection.php?error=3");
+		}elseif($_POST["mdp"]<5 || $_POST["mdp1"]<5){
+			header("location:./redirection.php?error=4");
+		}
+	}else{
+		header("location:./redirection.php?error=5");
 	}
 ?>
