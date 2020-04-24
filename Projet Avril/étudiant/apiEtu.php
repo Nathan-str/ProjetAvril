fa9a563f478fe08abac8e59baf441be602fd3d0a
Nathan Sestre <sestre.nathan@orange.fr>
2020-04-24 14:04:45 +0200

    Utilisation et vérification de la clé API

--- Projet Avril/étudiant/apiEtu.php
+++ Projet Avril/étudiant/apiEtu.php
@@ -37,16 +37,6 @@ function api(){
 
 function filiere(){
 
-	$fichier = fopen('fichiers/images.csv', 'r+');
-	for ($i=0;$i<sizeof(file("fichiers/images.csv"));$i++){
- 		$ligne = fgets($fichier);
- 		$lignes = substr($ligne, 0,-1);
-		$tableau = explode(";", $ligne);
-
-		$nom_image = $tableau[1];
-	}
-
-
 	$donnes = fopen('fichiers/comptes.csv', 'r+');
 	for ($i=0;$i<sizeof(file("fichiers/comptes.csv"));$i++){
  		$ligne = fgets($donnes);
@@ -59,46 +49,51 @@ function filiere(){
 			$jsonArray["$filiere"]["$tableau[8]"]["$tableau[0]"]["prenom"] = $tableau[2];
 			$jsonArray["$filiere"]["$tableau[8]"]["$tableau[0]"]["mail"] = $tableau[3];
 			$jsonArray["$filiere"]["$tableau[8]"]["$tableau[0]"]["numero"] = $tableau[4];
-			$jsonArray["$filiere"]["$tableau[8]"]["$tableau[0]"]["image"] = $nom_image;
+			$jsonArray["$filiere"]["$tableau[8]"]["$tableau[0]"]["image"] = $tableau[9];
 			$jsonArray["$filiere"]["$tableau[8]"]["$tableau[0]"]["id"] = $tableau[0];
 		}
 	}
+	fclose($donnes);
 	return $jsonArray;
 }
 
-function groupe(){
 
-	$donnes = fopen('fichiers/comptes.csv', 'r+');
-	for ($i=0;$i<sizeof(file("fichiers/comptes.csv"));$i++){
- 		$ligne = fgets($donnes);
- 		$lignes = substr($ligne, 0,-1);
-		$tableau = explode(";", $ligne);
-		$groupe = $tableau[8];
 
-			if ($_GET["filiere"] == $groupe){
-				$jsonArray["$groupe"]["$tableau[0]"]["nom"] = $tableau[1];
-				$jsonArray["$groupe"]["$tableau[0]"]["prenom"] = $tableau[2];
-				$jsonArray["$groupe"]["$tableau[0]"]["mail"] = $tableau[3];
-				$jsonArray["$groupe"]["$tableau[0]"]["numero"] = $tableau[4];
-				$jsonArray["$groupe"]["$tableau[0]"]["image"] = "none";
-				$jsonArray["$groupe"]["$tableau[0]"]["id"] = $tableau[0];
-			}
+$donne = fopen('fichiers/cle.csv', 'r+');
+
+while(!feof($donne)){
+	$ligne = fgets($donne);
+	if ($ligne != "") {
+		$lignes = substr($ligne, 0,-1);
+		$tableau = explode(";", $lignes);
+
+		if ($_GET['cle'] == $tableau[1]){
+			$validation = true;
+			break;
+		}else{
+			$continue = false;
+		}
 	}
-	return $jsonArray;
 }
+fclose($donne);
 
-if ($_GET["choix"] == "filiere"){
-	$json = filiere();
-	$json = json_encode($json);
-	header('Content-type: application/json');
-	echo($json);
-}elseif($_GET["choix"] == "groupe"){
-	$json = groupe();
-	$json = json_encode($json);
-	header('Content-type: application/json');
-	echo($json);
+if ($validation == true){
+	if ($_GET["choix"] == "filiere"){
+		$json = filiere();
+		$json = json_encode($json);
+		header('Content-type: application/json');
+		echo($json);
+	}elseif($_GET["choix"] == "groupe"){
+		$json = groupe();
+		$json = json_encode($json);
+		header('Content-type: application/json');
+		echo($json);
+	}
 }else{
-	echo "bonjour";
+	$jsonError = $jsonError["Error"] = "API KEY invalide";
+	$jsonError = json_encode($jsonError);
+	header('Content-type: application/json');
+	echo($jsonError);
 }
 
 
