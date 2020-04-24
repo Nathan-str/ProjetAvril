837ed4a4cd3a5f5c06ca017fb9888a0ca8ba2b98
Nathan Sestre <sestre.nathan@orange.fr>
2020-04-24 14:06:32 +0200

    Inscription de comptes pour recevoir la clé APÏ et envoie de la clé si la connexion s'effectue correctement

--- Projet Avril/étudiant/cle.php
+++ Projet Avril/étudiant/cle.php
@@ -12,9 +12,56 @@ function genererChaineAleatoire($longueur = 10){
 	 return $chaineAleatoire;
 }
 
+function verifCle(){
+	/*$donnes = fopen('fichiers/cle.csv', 'r+');
 
-$donne = fopen('fichiers/cle.csv', 'r+');
-	$_SESSION["mail"] = $_GET['key'];
+	for ($i=0;$i<sizeof(file("fichiers/cle.csv"));$i++){
+	 	$ligne = fgets($donnes);
+	 	$lignes = substr($ligne, 0,-1);
+		$tableau = explode(";", $lignes);
+
+		if ($_GET['key-mail'] == $tableau[0] && $_GET['key-pwd'] == $tableau[2]) {
+			$do = true;
+		}else{
+			$do = false;
+		}
+	}
+	fclose($donnes);*/
+
+	//if ($do == true) {
+		$donne = fopen('fichiers/cle.csv', 'r+');
+		$_SESSION["mail"] = $_GET['key-mail'];
+		$mot_de_passe = $_GET['key-pwd'];
+
+		while(!feof($donne)){
+			$ligne = fgets($donne);
+			if ($ligne != "") {
+				$lignes = substr($ligne, 0,-1);
+				$tableau = explode(";", $lignes);
+
+				if ($_GET['key-mail'] == $tableau[0] && hash("sha256",$mot_de_passe) == $tableau[2]){
+					$continue = true;
+					break;
+				}else{
+					$continue = false;
+				}
+			}
+		}
+
+		fclose($donne);
+
+		if ($continue == false){
+			header("location:./documentation.php?error=0");
+		}else{
+			header("location:./documentation.php?error=1");
+		}
+	/*}else{
+		header("location:./documentation.php?error=2");
+	}*/
+}
+
+function inserCle(){
+	$donne = fopen('fichiers/cle.csv', 'r+');
 
 	while(!feof($donne)){
 		$ligne = fgets($donne);
@@ -22,33 +69,48 @@ $donne = fopen('fichiers/cle.csv', 'r+');
 			$lignes = substr($ligne, 0,-2);
 			$tableau = explode(";", $lignes);
 
-			if ($_GET['key'] == $tableau[0]){
-				$continue = false;
+			if ($_GET['mail'] == $tableau[0]){
+				$fin = false;
 				break;
 			}else{
-				$continue = true;
+				$fin = true;
 			}
 		}
 	}
 
-fclose($donne);
+	if ($fin == true){
 
-if ($continue == true){
+		$mail = $_GET['mail'];
+		$password = $_GET['pwd'];
+		$cle = genererChaineAleatoire(20);
 
-	$fichier = fopen('fichiers/cle.csv', 'a');
+		//Vérifie si le formulaire a bien été rempli
+		if (isset($mail) && isset($password)){
 
-	//Generation de la clé
-	$cle = genererChaineAleatoire(20);
-	$_SESSION["cle"] = $cle;
-	$strinfos = $_GET['key'] . ";" . $cle . "\n";
-	fputs($fichier, $strinfos);
-	//Ecriture de la clé dans le fichier
-	//redirection avec la clé en GET
-	fclose($fichier);
-	header("location:./documentation.php?error=0");
-}else{
-	header("location:./documentation.php?error=1");
+			$donnes = fopen('fichiers/cle.csv', 'a+');
+			
+			//Met les éléments du formulaire dans le fichier et hache le mot de passe et définie un ID
+			$strinfos = $_GET['mail'] . ";" . $cle . ";" .hash("sha256",$password) ."\n";
+			fputs($donnes, $strinfos);
+
+			fclose($donnes);
+
+			header("location:./documentation.php?error=6");
+
+		}elseif($_GET['pwd'] != $_GET['pwd1']){
+			//Si il y a une erreur alors il est redirigé vers l'accueil
+			header("location:./documentation.php?error=3");
+		}elseif($_POST["pwd"]<5 || $_POST["pwd1"]<5){
+			header("location:./documentation.php?error=4");
+		}
+	}else{
+		header("location:./documentation.php?error=5");
+	}
 }
+
+//inserCle();
+//verifCle();
+
 
 
 
