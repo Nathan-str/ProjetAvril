872c235711169f9633374928ccf44f70bc6d50d8
Nathan Sestre <sestre.nathan@orange.fr>
2020-04-24 21:42:25 +0200

    Ajout d'une fonction pour afficher que les groupes

--- Projet Avril/étudiant/apiEtu.php
+++ Projet Avril/étudiant/apiEtu.php
@@ -34,6 +34,23 @@ function api(){
 
 }
 
+function ApiLogReussi($evenement){
+	$fichier = 'fichiers/log.csv';
+	$time = date("D, d M Y H:i:s");
+	$time = "[".$time."]";
+	$evenement = $time. ";" ."api_distribué".";". $evenement."\n";
+
+	file_put_contents($fichier, $evenement, FILE_APPEND);
+}
+
+function ApiLogEchec($evenement){
+	$fichier = 'fichiers/log.csv';
+	$time = date("D, d M Y H:i:s");
+	$time = "[".$time."]";
+	$evenement = $time. ";" ."echec_api".";". $evenement ."\n";
+
+	file_put_contents($fichier, $evenement, FILE_APPEND);
+}
 
 function filiere(){
 
@@ -57,6 +74,53 @@ function filiere(){
 	return $jsonArray;
 }
 
+function groupe($filiere, $groupe){
+
+    $donnes = fopen('fichiers/comptes.csv', 'r+');
+	for ($i=0;$i<sizeof(file("fichiers/comptes.csv"));$i++){
+ 		$ligne = fgets($donnes);
+ 		$lignes = substr($ligne, 0,-1);
+		$tableau = explode(";", $ligne);
+
+		if($filiere == $tableau[7] && $groupe == $tableau[8]){
+			$jsonArray["$filiere"]["$groupe"]["$tableau[0]"]["nom"] = $tableau[1];
+			$jsonArray["$filiere"]["$groupe"]["$tableau[0]"]["prenom"] = $tableau[2];
+			$jsonArray["$filiere"]["$groupe"]["$tableau[0]"]["mail"] = $tableau[3];
+			$jsonArray["$filiere"]["$groupe"]["$tableau[0]"]["numero"] = $tableau[4];
+			$jsonArray["$filiere"]["$groupe"]["$tableau[0]"]["image"] = $tableau[9];
+			$jsonArray["$filiere"]["$groupe"]["$tableau[0]"]["id"] = $tableau[0];
+		}
+
+	}
+	fclose($donnes);
+	return $jsonArray;
+    // ouvre le fichier CSV des etudiants
+
+    // parcourt chaque ligne 
+
+        // on "explode" la ligne
+        // on compare l'element 4 du tableau de la ligne avec la $filiere ET l'element 5 avec le $group
+        // si ok
+            
+
+
+
+    // Retourne un tableau contenant pour chaque entrée : un tableau avec toutes les infos de l'etudiant
+
+/*function transformArrayToJSON( $tab ){
+    return json_encode($tab);
+}
+
+
+$info = getAllStudentInfo( "LPI", "LPI-1" );
+
+$json = transformArrayToJSON($info);
+
+
+// print_r($info);
+echo($json);*/
+}
+
 
 
 $donne = fopen('fichiers/cle.csv', 'r+');
@@ -67,7 +131,7 @@ while(!feof($donne)){
 		$lignes = substr($ligne, 0,-1);
 		$tableau = explode(";", $lignes);
 
-		if ($_GET['cle'] == $tableau[1]){
+		if ($_GET['cle'] == $tableau[1]){	
 			$validation = true;
 			break;
 		}else{
@@ -78,18 +142,20 @@ while(!feof($donne)){
 fclose($donne);
 
 if ($validation == true){
+	ApiLogReussi($_GET['cle']);
 	if ($_GET["choix"] == "filiere"){
 		$json = filiere();
 		$json = json_encode($json);
 		header('Content-type: application/json');
 		echo($json);
 	}elseif($_GET["choix"] == "groupe"){
-		$json = groupe();
+		$json = groupe($_GET["filiere"], $_GET["groupe"]);
 		$json = json_encode($json);
 		header('Content-type: application/json');
 		echo($json);
 	}
 }else{
+	ApiLogEchec($_GET['cle']);
 	$jsonError = $jsonError["Error"] = "API KEY invalide";
 	$jsonError = json_encode($jsonError);
 	header('Content-type: application/json');
