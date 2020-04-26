8c059057793e84dc98fc1f0d237999c0ee3a578b
Nathan Sestre <sestre.nathan@orange.fr>
2020-04-26 17:00:20 +0200

    Ajout d'un menu déroulant adaptable pour les groupes selon les filières sélectionnées

--- Projet Avril/étudiant/redirection.php
+++ Projet Avril/étudiant/redirection.php
@@ -45,7 +45,7 @@ include 'fonction.php';
 				</nav>
 		</header>
 
-		<script src="app.js" meta="utf-8"></script>
+		
 
 		<div class="page-wrapper">
 
@@ -62,23 +62,36 @@ include 'fonction.php';
 					<input class="input" type="password" name="mdp" minlength="6" placeholder="Mot de passe" required="required" />
 					<input class="input" type="password" name="mdp1" minlength="6" placeholder="Confirmation mot de passe" required="required" />
 					<input class="submit" type="submit" value="Valider" />
-					<p>Filière:</p>
-					<input class="radio" type="radio" name="filiere" value="L1-MIPI" checked=""/>L1-MIPI
-					<input class="radio" type="radio" name="filiere" value="L2-MI" />L2-MI
-					<input class="radio" type="radio" name="filiere" value="L3-I" />L3-I
-					<input class="radio" type="radio" name="filiere" value="LP-RS" />LP-RS
-					<input class="radio" type="radio" name="filiere" value="LPI-RIWS" />LPI-RIWS
-					<p>Groupe:</p>
-					<input type="radio" name="groupe" value="A1" checked=""/>A1
-					<input type="radio" name="groupe" value="B2" />B2
-					<input type="radio" name="groupe" value="LPI-1" />LPI-1
-					<input type="radio" name="groupe" value="LPI-2" />LPI-2
-					<input type="radio" name="groupe" value="LPI-3" />LPI-3
+					<p id="filiere-p">Filière</p>
+					<input class="radio" type="radio" onclick="selection()" name="filiere" id="L1-MIPI" value="L1-MIPI" checked=""/>L1-MIPI
+					<input class="radio" type="radio" onclick="selection()" name="filiere" id="L2-MI" value="L2-MI" />L2-MI
+					<input class="radio" type="radio" onclick="selection()" name="filiere" id="L3-I" value="L3-I" />L3-I
+					<input class="radio" type="radio" onclick="selection()" name="filiere" id="LP-RS" value="LP-RS" />LP-RS
+					<input class="radio" type="radio" onclick="selection()" name="filiere" id="LPI-RIWS" value="LPI-RIWS" />LPI-RIWS
+					<p id="groupe-p">Groupe</p>
+					<select name="groupe" id="groupe">
+						<option id="A1" style='display:none;'>A1</option>
+						<option id="A2" style='display:none;'>A2</option>
+						<option id="A3" style='display:none;'>A3</option>
+						<option id="B1" style='display:none;'>B1</option>
+						<option id="B2" style='display:none;'>B2</option>
+						<option id="B3" style='display:none;'>B3</option>
+						<option id="C1" style='display:none;'>C1</option>
+						<option id="C2" style='display:none;'>C2</option>
+						<option id="C3" style='display:none;'>C3</option>
+						<option id="D1" style='display:none;'>D1</option>
+						<option id="D2" style='display:none;'>D2</option>
+						<option id="D3" style='display:none;'>D3</option>
+						<option id="E1" style='display:none;'>E1</option>
+						<option id="E2" style='display:none;'>E2</option>
+						<option id="E3" style='display:none;'>E3</option>
+					</select>
 				</form>
 				<?php
 					errorInscription();
 				?>
 			</div>
+
 
 			<div class="form_connexion">
 				<p class="p_connexions">Connexion</p>
@@ -94,7 +107,7 @@ include 'fonction.php';
 
 		</div>
 
-	
+	<script src="app.js" meta="utf-8"></script>
 
 		<footer class="le_footer">
 			<div class="contenue">
