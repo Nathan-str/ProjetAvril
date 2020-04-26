3e6fc1d1c7f242f8c0bbc50134e29a248d12d8bf
Nathan Sestre <sestre.nathan@orange.fr>
2020-04-26 16:59:11 +0200

    Ajout des logs pour l'inscription et définition de la fonction pour vérifier les mails doubles et les numéros

--- Projet Avril/étudiant/inscription.php
+++ Projet Avril/étudiant/inscription.php
@@ -3,27 +3,9 @@
 
 	include 'fonction.php';
 
-	$donne = fopen('fichiers/comptes.csv', 'r+');
 
-	while(!feof($donne)){
-		$ligne = fgets($donne);
-		if ($ligne != "") {
-			$lignes = substr($ligne, 0,-2);
-			$tableau = explode(";", $lignes);
 
-			if ($_POST['mail'] == $tableau[3]){
-				$fin = false;
-				break;
-			}else{
-				$fin = true;
-			}if ($_POST['numero'] == $tableau[4]) {
-				$fin = false;
-				break;
-			}else{
-				$fin = true;
-			}
-		}
-	}
+	$fin = verification($_POST['mail'], $_POST['numero'] , 'fichiers/comptes.csv');
 
 	if ($fin == true){
 
@@ -33,7 +15,7 @@
 		$secure_mot_de_passe = $car_alea . $mot_de_passe;
 
 		//Vérifie si le formulaire a bien été rempli
-		if (isset($_POST['nom']) && isset($_POST['prenom']) && isset($_POST['mail']) && isset($_POST['mdp']) && isset($_POST['mdp1']) && !empty($_POST["nom"]) && !empty($_POST['prenom']) && !empty($_POST['mail']) && !empty($_POST['mdp']) && !empty($_POST['mdp1']) && $_POST['mdp'] == $_POST['mdp1']){
+		if (isset($_POST['nom']) && isset($_POST['prenom']) && isset($_POST['mail']) && isset($_POST['mdp']) && isset($_POST['mdp1']) && isset($_POST['filiere']) && !empty($_POST["nom"]) && !empty($_POST['prenom']) && !empty($_POST['mail']) && !empty($_POST['mdp']) && !empty($_POST['mdp1']) && $_POST['mdp'] == $_POST['mdp1'] && !empty($_POST['filiere'])){
 
 			$donnes = fopen('fichiers/comptes.csv', 'a+');
 			$monfichier = fopen('fichiers/id.txt', 'r+');
@@ -50,16 +32,19 @@
 			fclose($monfichier);
 			fclose($donnes);
 
-			inscriptionLogReussi($_POST["mail"]);
+			FichierLog("inscription réussi",$_POST["mail"]);
 			header("location:./redirection.php?error=0");
 
 		}elseif($_POST["mdp"] != $_POST["mdp1"]){
 			//Si il y a une erreur alors il est redirigé vers l'accueil
+			FichierLog("inscription échoué",$_POST["mail"]);
 			header("location:./redirection.php?error=3");
 		}elseif($_POST["mdp"]<5 || $_POST["mdp1"]<5){
+			FichierLog("inscription échoué",$_POST["mail"]);
 			header("location:./redirection.php?error=4");
 		}
-	}else{
+	}elseif ($continue == false){
+		FichierLog("inscription échoué",$_POST["mail"]);
 		header("location:./redirection.php?error=5");
 	}
 ?>
