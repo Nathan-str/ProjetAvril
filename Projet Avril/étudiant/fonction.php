d4c59ada557e1c1386db19061da5c871e6b959c5
Nathan Sestre <sestre.nathan@orange.fr>
2020-04-27 19:46:40 +0200

    Définition d'un compteur pour chaque étudiant dans l'API Amélioration des envoies de requête pour les filieres et pour les groupes

--- Projet Avril/étudiant/fonction.php
+++ Projet Avril/étudiant/fonction.php
@@ -388,41 +388,6 @@
 	//Fonction pour la page "apiEtu.php"
 
 
-	//Fonction permettant d'afficher l'API pour tous les comptes étudiants (finalement pas utilisé car affiche trop d'informations)
-	//Fonction utilisé pour la page "apiEtu.php"
-	function api(){
-		header('Content-type: application/json');
-		$donnes = fopen('fichiers/comptes.csv', 'r+');
-
-		for ($i=0;$i<sizeof(file("fichiers/comptes.csv"));$i++){
-	 		$ligne = fgets($donnes);
-	 		$lignes = substr($ligne, 0,-1);
-			$tableau = explode(";", $ligne);
-
-			$jsonArray["$tableau[7]"]["$tableau[8]"]["$tableau[0]"]["nom"] = $tableau[1];
-			$jsonArray["$tableau[7]"]["$tableau[8]"]["$tableau[0]"]["prenom"] = $tableau[2];
-			$jsonArray["$tableau[7]"]["$tableau[8]"]["$tableau[0]"]["mail"] = $tableau[3];
-			$jsonArray["$tableau[7]"]["$tableau[8]"]["$tableau[0]"]["numero"] = $tableau[4];
-			$jsonArray["$tableau[7]"]["$tableau[8]"]["$tableau[0]"]["image"] = "none";
-			$jsonArray["$tableau[7]"]["$tableau[8]"]["$tableau[0]"]["id"] = $tableau[0];
-
-		}
-
-		fclose($donnes);
-		//array_multisort($jsonArray["$tableau[7]"]["$tableau[8]"]);
-		//asort($jsonArray["$tableau[7]"]["$tableau[8]"]);
-		
-		ksort($jsonArray);
-
-		/*$fichierJson = fopen("api/api_etudiant.json", "w");
-	        $jsonArray = json_encode($jsonArray);
-	        fwrite($fichierJson, $jsonArray);*/
-
-	    $jsonArray = json_encode($jsonArray);
-	    echo($jsonArray);
-
-	}
-
 	//Fonction permettant d'écrire si l'envoie d'API a fonctionner dans le fichier des logs avec evenement la clé d'API
 	//Fonction utilisé sur la page "apiEtu.php"
 	function ApiLogReussi($evenement){
@@ -451,23 +416,33 @@
 	function filiere(){
 
 		$donnes = fopen('fichiers/comptes.csv', 'r+');
+		$jsonTableau = array();
+		$jsonArray = array();
+		$cpt = 0;
+		
 		for ($i=0;$i<sizeof(file("fichiers/comptes.csv"));$i++){
 	 		$ligne = fgets($donnes);
-	 		$lignes = substr($ligne, 0,-1);
+	 		$lignes = substr($ligne, 0,-2);
 			$tableau = explode(";", $ligne);
 			$filiere = $tableau[7];
+			$groupe = $tableau[8];
+			$tailleTableau = sizeof($jsonTableau)+1;
 			
 			if ($_GET["filiere"] == $filiere){
-				$jsonArray["$filiere"]["$tableau[8]"]["$tableau[0]"]["nom"] = $tableau[1];
-				$jsonArray["$filiere"]["$tableau[8]"]["$tableau[0]"]["prenom"] = $tableau[2];
-				$jsonArray["$filiere"]["$tableau[8]"]["$tableau[0]"]["mail"] = $tableau[3];
-				$jsonArray["$filiere"]["$tableau[8]"]["$tableau[0]"]["numero"] = $tableau[4];
-				$jsonArray["$filiere"]["$tableau[8]"]["$tableau[0]"]["image"] = $tableau[9];
-				$jsonArray["$filiere"]["$tableau[8]"]["$tableau[0]"]["id"] = $tableau[0];
+				$cpt +=1;
+				$jsonArray["groupe"] = $groupe;
+				$jsonArray["nom"] = $tableau[1];
+				$jsonArray["prenom"] = $tableau[2];
+				$jsonArray["mail"] = $tableau[3];
+				$jsonArray["numero"] = $tableau[4];
+				$jsonArray["image"] = $tableau[9];
+				$jsonArray["id"] = $tableau[0];
+				$jsonTableau["$filiere"][$cpt] = $jsonArray;
+
 			}
 		}
 		fclose($donnes);
-		return $jsonArray;
+		return $jsonTableau;
 	}
 
 
@@ -476,24 +451,31 @@
 	function groupe($filiere, $groupe){
 
 	    $donnes = fopen('fichiers/comptes.csv', 'r+');
+	    $jsonTableau = array();
+	    $cpt = 0;
 		for ($i=0;$i<sizeof(file("fichiers/comptes.csv"));$i++){
 	 		$ligne = fgets($donnes);
 	 		$lignes = substr($ligne, 0,-1);
 			$tableau = explode(";", $ligne);
 
 			if($filiere == $tableau[7] && $groupe == $tableau[8]){
-				$jsonArray["$filiere"]["$groupe"]["$tableau[0]"]["nom"] = $tableau[1];
-				$jsonArray["$filiere"]["$groupe"]["$tableau[0]"]["prenom"] = $tableau[2];
-				$jsonArray["$filiere"]["$groupe"]["$tableau[0]"]["mail"] = $tableau[3];
-				$jsonArray["$filiere"]["$groupe"]["$tableau[0]"]["numero"] = $tableau[4];
-				$jsonArray["$filiere"]["$groupe"]["$tableau[0]"]["image"] = $tableau[9];
-				$jsonArray["$filiere"]["$groupe"]["$tableau[0]"]["id"] = $tableau[0];
+				$cpt += 1;
+				$jsonArray["nom"] = $tableau[1];
+				$jsonArray["prenom"] = $tableau[2];
+				$jsonArray["mail"] = $tableau[3];
+				$jsonArray["numero"] = $tableau[4];
+				$jsonArray["image"] = $tableau[9];
+				$jsonArray["id"] = $tableau[0];
+				$jsonTableau["$filiere"]["$groupe"][$cpt] = $jsonArray;
+
 			}
 
 		}
 		fclose($donnes);
-		return $jsonArray;
+		return $jsonTableau;
 	}
+
+	
 
 
 
