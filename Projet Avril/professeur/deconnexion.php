e3722e5710e2bd84aa4a6e55ba6805ad2b64882f
Nathan Sestre <sestre.nathan@orange.fr>
2020-04-27 19:28:34 +0200

    Page servant à se déconnecter de la session active (faire en fonction)

---
+++ Projet Avril/professeur/deconnexion.php
@@ -0,0 +1,13 @@
+<?php
+	session_start();
+
+	include 'fonction.php';
+
+	//Détruit la session déjà existante
+	FichierLog("déconnexion",$_SESSION['pseudo']);
+	session_destroy();
+
+	setcookie('pseudo',"",1);
+	
+	header("location: ./index.php");
+?>
