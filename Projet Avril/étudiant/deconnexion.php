af7d8964877d31dead63f4b8a31d27ce086f1a31
Nathan Sestre <sestre.nathan@orange.fr>
2020-04-27 19:44:31 +0200

    Reglage des problèmes d'écriture en PHP et donc des bugs

--- Projet Avril/étudiant/deconnexion.php
+++ Projet Avril/étudiant/deconnexion.php
@@ -1,8 +1,10 @@
 <?php
 	session_start();
 
+	include 'fonction.php';
+
 	//Détruit la session déjà existante
-	FichierLog("déconnexion",$_SESSION['pseudo'];
+	FichierLog("déconnexion",$_SESSION['pseudo']);
 	session_destroy();
 
 	setcookie('pseudo',"",1);
