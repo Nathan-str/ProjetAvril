7c052d8a1088f176a2b85da9f01b2310ba99e2e3
Nathan Sestre <sestre.nathan@orange.fr>
2020-04-27 19:31:01 +0200

    Ajout des messages d'erreur si il y a des problèmes avec la redirection Ajout d'un header et des links pour le css Utilisation des cookies pour accèder à la derniere recherche

--- Projet Avril/professeur/demande.php
+++ Projet Avril/professeur/demande.php
@@ -1,26 +1,77 @@
 <?php
 session_start();
 
+if(isset($_GET['error'])){
+	if($_GET['error'] == 1){ //2: GET définie dans la page vérifiant les identifiants 
+	?>
+	<script type="text/javascript">
+		alert("Aucune clé n'est renseigné !")
+	</script>
+	<?php
+	}elseif($_GET['error'] == 2)	{
+	?>
+	<script type="text/javascript">
+		alert("Aucun groupe n'est indiqué !")
+	</script>
+	<?php	
+	}		
+}
+
+
+
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Trombinoscope</title>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" charset="utf-8"></script>
+	<link rel="stylesheet" type="text/css" href="styles.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 </head>
 <body>
+	<header role="header" >	
+			<nav class="menu" role="navigation">
+				<div class="inner">
+					<div class="gauche">
+						<a class="logo" href="index.php">Trombinoscope</a>
+					</div>
 
+					<div class="droite">
+						
+						<!--<a href="#" class="lien"><i class="fa fa-globe"></i> A propos</a>-->
+						<?php
+							if(!empty($_SESSION['pseudo'])){
+						?>
+								<a href="#" class="lien"><i class='fa fa-book'></i> mosaique</a>
+								<a href="deconnexion.php" class="lien"><i class="fa fa-sign-out"></i> Déconnexion</a>
+						<?php
+							}
+						?>
+
+					</div>
+
+					<div class="nav-toggle">
+						<span class="toggle-icons"></span>
+					</div>
+
+				</div>
+			</nav>
+	</header>
+<?php
+if (isset($_SESSION['pseudo'])){
+?>
 
 <div id="formulaire-API">
-	<p>Accédez à l'API</p>
-	<form action="traitement.php" method="post" class="formulaire-API1">
+	<p>Accédez à la mosaïque</p>
+	<form action="traitement.php" method="get" class="formulaire-API1">
 
 		<input type="radio" name="choix" onclick="change()" value="filiere" id="choice" checked="" />Filière
 		<input class="filiere" type="radio" onclick="selection()" name="filiere" id="L1-MIPI" checked="" value="L1-MIPI" />L1-MIPI
 		<input class="filiere" type="radio" onclick="selection()" name="filiere" id="L2-MI" value="L2-MI" />L2-MI
 		<input class="filiere" type="radio" onclick="selection()" name="filiere" id="L3-I" value="L3-I" />L3-I
-		<input class="filiere" type="radio" onclick="selection()" name="filiere" id="LP-RS" value="LP RS" />LP-RS
+		<input class="filiere" type="radio" onclick="selection()" name="filiere" id="LP-RS" value="LP-RS" />LP-RS
 		<input class="filiere" type="radio" onclick="selection()" name="filiere" id="LPI-RIWS" value="LPI-RIWS" />LPI-RIWS
 
 		<input type="radio" name="choix" onclick="change()" id="choice1" value="groupe" />Groupe
@@ -44,16 +95,31 @@ session_start();
 
 		<input type="text" name="cle" placeholder="Clé API" />
 		<input class="form-cle-api" type="submit" value="Valider">
+
 	</form>
 
 
 </div>
+
+<?php
+if (isset($_COOKIE["recherche"])){
+	$jasonRecherche = $_COOKIE["recherche"];
+	$recherche = json_decode($jasonRecherche, True);
+	echo "<a id=\"derniere_href\" href='traitement.php?filiere=" . $recherche["filiere"] . "&amp;" ."choix=groupe" . "&amp;" . "groupe=" . $recherche["groupe"] . "&amp;" . "cle=" . $recherche["cle"] ."'>Dernière recherche d'un groupe</a>";
+	//http://nathan-str-prof.alwaysdata.net/traitement.php?filiere=LP-RS&choix=groupe&groupe=D1&cle=U0UWfUlUhxvmOg1HTZZQ
+}else{
+	echo"bonjour";
+}
+
+?>
+
 <script src="app.js" meta="utf-8"></script>
 
-
-<p>Bonjour</p>
-
-
+<?php
+}else{
+	header("location: ./index.php");
+}
+?>
 </body>
 </html>
 
