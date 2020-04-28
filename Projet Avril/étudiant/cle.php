fc3a1fc5544c9bd83dd9e2b3d27343135d4aee09
Nathan Sestre <sestre.nathan@orange.fr>
2020-04-28 21:22:44 +0200

    Ajout du compteur de la clé pour son affichage de son nombre d'utilisation sans ajouter de valeur, pour le renvoie de texte

--- Projet Avril/étudiant/cle.php
+++ Projet Avril/étudiant/cle.php
@@ -3,9 +3,42 @@ session_start();
 
 include 'fonction.php';
 
+function compteurCleAffichage(){
+		$donnes = fopen('fichiers/cle.csv', 'r+');
+		$informations = array();
+
+		for ($i=0;$i<sizeof(file("fichiers/cle.csv"));$i++){
+		 		$ligne = fgets($donnes);
+		 		$lignes = substr($ligne, 0,-1);
+				$tableau = explode(";", $lignes);
+				$temps_actuel = time();
+				$temps = date('h',$temps_actuel);
+
+				if ($temps == $tableau[3]) {
+					$cpt = $tableau[4];
+					$time = $tableau[3];
+				}else{
+					$time = $temps;
+					$cpt = 1;
+				}	
+				$strinformations = $tableau[0] . ";" . $tableau[1] . ";" . $tableau[2] . ";" . $time . ";" . $cpt;
+				array_push($informations, $strinformations);
+				
+		}
+
+		fclose($donnes);
+		$donnes = fopen('fichiers/cle.csv', 'w');
+
+		for ($i=0;$i<sizeof($informations);$i++){
+			fputs($donnes, $informations[$i] . "\n");
+		}
+		fclose($donnes);
+	}
+
 if ($_GET['choose'] == "inscription"){
 	inserCle();
 }elseif ($_GET['choose'] == "connexion") {
+	compteurCleAffichage();
 	verifCle();
 }else{
 	echo"error";
