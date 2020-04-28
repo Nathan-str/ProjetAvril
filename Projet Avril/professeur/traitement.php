be6bf322cf6ba4c5a9ebd19aaa3633bbbe43baf5
Nathan Sestre <sestre.nathan@orange.fr>
2020-04-28 21:29:34 +0200

    Mise en fonction de l'affichage de la mosaïque

--- Projet Avril/professeur/traitement.php
+++ Projet Avril/professeur/traitement.php
@@ -4,67 +4,76 @@
 //$jsonArray = json_decode($jsonText,True);
 //print_r($jsonArray['LPI-RIWS']['E3']['13']['prenom']);//Exemple: $jsonArray['$_POST[filiere]']['$_POST[groupe]']['id(pas sur)']['prenom']
 
-$filiere = $_GET['filiere'];
-$groupe = $_GET['groupe'];
-$cle = $_GET['cle'];
+//function genereEleve(){
 
-if ($_GET['choix'] == "filiere"){
-	if (!empty($cle)){
-		$jsonArray = array();
-		$jsonText = file_get_contents('http://nathan-str-etudiant.alwaysdata.net/apiEtu.php?choix=filiere&filiere='.$filiere.'&cle=' .$cle);
-		$jsonArray = json_decode($jsonText,True);
-		$cpt = 0;
+//}
 
-		for ($i=1; $i <= sizeof($jsonArray["$filiere"]); $i++){ //A mettre plus tard le sizeof du post voulu
-		//<img src=$jsonArray['$_POST[filiere]']['$_POST[groupe]']['id']['image']
-			$cpt += 1;
-			echo("<div class=profil>");
-			echo("<img src=http://nathan-str-etudiant.alwaysdata.net/images/" . $jsonArray["$filiere"][$i]['image'] . "width=170 height=170 style=border-radius:10px; alt=error class=image><br />");
-			echo $jsonArray["$filiere"][$i]['prenom'] . " " . $jsonArray["$filiere"][$i]['nom'] . "<br />";
-			//echo $jsonArray["$filiere"][$i]['mail'] . "<br />";
-			//echo $jsonArray["$filiere"][$i]['numero'] . "<br />";
-			echo("</div>");
-			
-		}
-	}else{
-		header("location:./demande.php?error=1");
-	}
-	
-}else{
+function mosaique(){
+	$filiere = $_GET['filiere'];
+	$groupe = $_GET['groupe'];
+	$cle = $_GET['cle'];
 
-	if(!empty($groupe)){
+	if ($_GET['choix'] == "filiere"){
 		if (!empty($cle)){
-
-			$recherche["filiere"] = $filiere;
-			$recherche["groupe"] = $groupe;
-			$recherche["cle"] = $cle;
-			$jasonRecherche = json_encode($recherche);
-			setcookie("recherche", $jasonRecherche);
-
 			$jsonArray = array();
-			$jsonText = file_get_contents('http://nathan-str-etudiant.alwaysdata.net/apiEtu.php?filiere='. $filiere .'&choix=groupe&groupe='.$groupe .'&cle='.$cle);
+			$jsonText = file_get_contents('http://nathan-str-etudiant.alwaysdata.net/apiEtu.php?choix=filiere&filiere='.$filiere.'&cle=' .$cle);
 			$jsonArray = json_decode($jsonText,True);
 			$cpt = 0;
 
-			for ($i=1; $i <= sizeof($jsonArray["$filiere"]["$groupe"]); $i++){
+			
+			for ($i=1; $i <= sizeof($jsonArray["$filiere"]); $i++){ //A mettre plus tard le sizeof du post voulu
+			//<img src=$jsonArray['$_POST[filiere]']['$_POST[groupe]']['id']['image']
 				$cpt += 1;
 				echo("<div class=profil>");
-				echo("<img src=http://nathan-str-etudiant.alwaysdata.net/images/" . $jsonArray["$filiere"]["$groupe"][$i]['image'] . "width=170 height=170 style=border-radius:10px; alt=error ><br />");
-				echo $jsonArray["$filiere"]["$groupe"][$i]['prenom'] . " " . $jsonArray["$filiere"]["$groupe"][$i]['nom'] . "<br />";
-				echo $jsonArray["$filiere"]["$groupe"][$i]['mail'] . "<br />";
-				echo $jsonArray["$filiere"]["$groupe"][$i]['numero'] . "<br />";
+				echo("<img src=http://nathan-str-etudiant.alwaysdata.net/images/" . $jsonArray["$filiere"][$i]['image'] . "width=170 height=170 style=border-radius:10px; alt=error class=image><br />");
+				echo $jsonArray["$filiere"][$i]['prenom'] . " " . $jsonArray["$filiere"][$i]['nom'] . "<br />";
+				//echo $jsonArray["$filiere"][$i]['mail'] . "<br />";
+				//echo $jsonArray["$filiere"][$i]['numero'] . "<br />";
 				echo("</div>");
-
 				
-
 			}
 		}else{
 			header("location:./demande.php?error=1");
 		}
+		
 	}else{
-		header("location:./demande.php?error=2");
+
+		if(!empty($groupe)){
+			if (isset($cle)){
+
+				$recherche["filiere"] = $filiere;
+				$recherche["groupe"] = $groupe;
+				$recherche["cle"] = $cle;
+				$jasonRecherche = json_encode($recherche);
+				setcookie("recherche", $jasonRecherche);
+
+				$jsonArray = array();
+				$jsonText = file_get_contents('http://nathan-str-etudiant.alwaysdata.net/apiEtu.php?filiere='. $filiere .'&choix=groupe&groupe='.$groupe .'&cle='.$cle);
+				$jsonArray = json_decode($jsonText,True);
+				$cpt = 0;
+
+				echo("<h1>Elèves du groupe " . $groupe . " de la filière " . $filiere . ":</h1>");
+
+				for ($i=1; $i <= sizeof($jsonArray["$filiere"]["$groupe"]); $i++){
+					$cpt += 1;
+					echo("<div class=profil>");
+					echo("<img src=http://nathan-str-etudiant.alwaysdata.net/images/" . $jsonArray["$filiere"]["$groupe"][$i]['image'] . "width=200 height=200 style=border-radius:10px; alt=error class=image onclick=\"clickImage($i)\";><br />");
+					echo "<p>".$jsonArray["$filiere"]["$groupe"][$i]['prenom'] . " " . $jsonArray["$filiere"]["$groupe"][$i]['nom'] . "</p><br />";
+					echo "<p class=info id=$i style=\"display: none;\">".$jsonArray["$filiere"]["$groupe"][$i]['mail'] . "<br />". $jsonArray["$filiere"]["$groupe"][$i]['numero'] . "</p><br />";
+					echo("</div>");
+
+					
+
+				}
+			}else{
+				header("location:./demande.php?error=1");
+			}
+		}else{
+			header("location:./demande.php?error=2");
+		}
 	}
 }
+
 //}elseif(isset($_POST['groupe'])){
 	//$jsonText = file_get_contents('URL pour le groupe');
 		//$jsonArray = json_decode($jsonText,True);
@@ -80,9 +89,47 @@ if ($_GET['choix'] == "filiere"){
 <html>
 <head>
 	<title>trombinoscope</title>
-	<link rel="stylesheet" type="text/css" href="styles.css">
+	<link rel="stylesheet" type="text/css" href="styles.css" media="screen">
+	<link rel="stylesheet" type="text/css" href="print.css" media="print">
 </head>
 <body>
+	<header role="header" >	
+			<nav class="menu" role="navigation">
+				<div class="inner">
+					<div class="gauche">
+						<a class="logo" href="index.php">Trombinoscope</a>
+					</div>
 
+					<div class="droite">
+						
+						<!--<a href="#" class="lien"><i class="fa fa-globe"></i> A propos</a>-->
+						<?php
+							if(!empty($_SESSION['pseudo'])){
+						?>
+								<a href="#" class="lien"><i class='fa fa-book'></i> mosaique</a>
+								<a href="deconnexion.php" class="lien"><i class="fa fa-sign-out"></i> Déconnexion</a>
+						<?php
+							}
+						?>
+
+					</div>
+
+					<div class="nav-toggle">
+						<span class="toggle-icons"></span>
+					</div>
+
+				</div>
+			</nav>
+	</header>
+
+
+<form>
+	<input type="button" class="impression" value="Imprimer" onClick="window.print()">
+</form>
+<?php
+mosaique();
+?>
+
+<script src="app.js" meta="utf-8"></script>
 </body>
 </html>
