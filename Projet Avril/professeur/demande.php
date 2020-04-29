a3b94ade938e5caeb3fa2715afbd084705976297
Nathan Sestre <sestre.nathan@orange.fr>
2020-04-29 23:57:46 +0200

    Ajout d'une fonction pour l'affichage en cas d'erreur du formulaire Ajout d'une fonction en cas d'erreur de session style du formulaire

--- Projet Avril/professeur/demande.php
+++ Projet Avril/professeur/demande.php
@@ -1,21 +1,29 @@
 <?php
 session_start();
 
-if(isset($_GET['error'])){
-	if($_GET['error'] == 1){ //2: GET définie dans la page vérifiant les identifiants 
-	?>
-	<script type="text/javascript">
-		alert("Aucune clé n'est renseigné !")
-	</script>
-	<?php
-	}elseif($_GET['error'] == 2)	{
-	?>
-	<script type="text/javascript">
-		alert("Aucun groupe n'est indiqué !")
-	</script>
-	<?php	
-	}		
+include 'fonction.php';
+
+verifSession($_SESSION['pseudo']);
+
+function errorMosaique(){
+	if(isset($_GET['error'])){
+		if($_GET['error'] == 1){ //2: GET définie dans la page vérifiant les identifiants 
+		?>
+		<script type="text/javascript">
+			alert("Aucune clé n'est renseigné !")
+		</script>
+		<?php
+		}elseif($_GET['error'] == 2)	{
+		?>
+		<script type="text/javascript">
+			alert("Aucun groupe n'est indiqué !")
+		</script>
+		<?php	
+		}		
+	}
 }
+
+errorMosaique();
 
 
 
@@ -24,7 +32,7 @@ if(isset($_GET['error'])){
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Trombinoscope</title>
+	<title>Administration</title>
 	<meta charset="utf-8" />
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" charset="utf-8"></script>
 	<link rel="stylesheet" type="text/css" href="styles.css">
@@ -36,7 +44,7 @@ if(isset($_GET['error'])){
 			<nav class="menu" role="navigation">
 				<div class="inner">
 					<div class="gauche">
-						<a class="logo" href="index.php">Trombinoscope</a>
+						<a class="logo" href="index.php">Administration</a>
 					</div>
 
 					<div class="droite">
@@ -60,44 +68,68 @@ if(isset($_GET['error'])){
 				</div>
 			</nav>
 	</header>
-<?php
-if (isset($_SESSION['pseudo'])){
-?>
+
+
+<h1 class="h1-demande">Accès à la mosaïque</h1>
+
 
 <div id="formulaire-API">
-	<p>Accédez à la mosaïque</p>
-	<form action="traitement.php" method="get" class="formulaire-API1">
+		<form action="traitement.php" method="get" class="formulaire-API1">
 
-		<input type="radio" name="choix" onclick="change()" value="filiere" id="choice" checked="" />Filière
-		<input class="filiere" type="radio" onclick="selection()" name="filiere" id="L1-MIPI" checked="" value="L1-MIPI" />L1-MIPI
-		<input class="filiere" type="radio" onclick="selection()" name="filiere" id="L2-MI" value="L2-MI" />L2-MI
-		<input class="filiere" type="radio" onclick="selection()" name="filiere" id="L3-I" value="L3-I" />L3-I
-		<input class="filiere" type="radio" onclick="selection()" name="filiere" id="LP-RS" value="LP-RS" />LP-RS
-		<input class="filiere" type="radio" onclick="selection()" name="filiere" id="LPI-RIWS" value="LPI-RIWS" />LPI-RIWS
+			<div class="div-radio-demande">
+				<input type="radio" name="choix" onclick="change()" value="filiere" id="choice" class="radio-demande" checked="" /><label class="label-demande">Filière:</label>
+			</div>
 
-		<input type="radio" name="choix" onclick="change()" id="choice1" value="groupe" />Groupe
-		<select name="groupe" id="groupe" style='display:none;'>
-			<option id="A1" style='display:none;'>A1</option>
-			<option id="A2" style='display:none;'>A2</option>
-			<option id="A3" style='display:none;'>A3</option>
-			<option id="B1" style='display:none;'>B1</option>
-			<option id="B2" style='display:none;'>B2</option>
-			<option id="B3" style='display:none;'>B3</option>
-			<option id="C1" style='display:none;'>C1</option>
-			<option id="C2" style='display:none;'>C2</option>
-			<option id="C3" style='display:none;'>C3</option>
-			<option id="D1" style='display:none;'>D1</option>
-			<option id="D2" style='display:none;'>D2</option>
-			<option id="D3" style='display:none;'>D3</option>
-			<option id="E1" style='display:none;'>E1</option>
-			<option id="E2" style='display:none;'>E2</option>
-			<option id="E3" style='display:none;'>E3</option>
-		</select>
+			<div class="div-radio-demande">
+				<input class="filiere" type="radio" onclick="selection()" name="filiere" id="L1-MIPI" class="radio-demande" checked="" value="L1-MIPI" /><label class="label-demande">L1-MIPI</label>
+			</div>
 
-		<input type="text" name="cle" placeholder="Clé API" />
-		<input class="form-cle-api" type="submit" value="Valider">
+			<div class="div-radio-demande">
+				<input class="filiere" type="radio" onclick="selection()" name="filiere" id="L2-MI" class="radio-demande" value="L2-MI" /><label class="label-demande">L2-MI</label>
+			</div>
+			
+			<div class="div-radio-demande">
+				<input class="filiere" type="radio" onclick="selection()" name="filiere" id="L3-I" class="radio-demande" value="L3-I" /><label class="label-demande">L3-I</label>
+			</div>
+			
+			<div class="div-radio-demande">
+				<input class="filiere" type="radio" onclick="selection()" name="filiere" id="LP-RS" class="radio-demande" value="LP-RS" /><label class="label-demande">LP-RS</label>
+			</div>
+			
+			<div class="div-radio-demande">
+				<input class="filiere" type="radio" onclick="selection()" name="filiere" id="LPI-RIWS" class="radio-demande" value="LPI-RIWS" /><label class="label-demande">LPI-RIWS</label>
+			</div>
+			
+			<div class="div-radio-demande">
+				<input type="radio" name="choix" onclick="change()" id="choice1" class="radio-demande" value="groupe" /><label class="label-demande">Groupe:</label>
+			</div>
 
-	</form>
+			<div class="div-radio-demande-liste">
+				<select name="groupe" id="groupe" style='display:none;'>
+					<option id="A1" style='display:none;'>A1</option>
+					<option id="A2" style='display:none;'>A2</option>
+					<option id="A3" style='display:none;'>A3</option>
+					<option id="B1" style='display:none;'>B1</option>
+					<option id="B2" style='display:none;'>B2</option>
+					<option id="B3" style='display:none;'>B3</option>
+					<option id="C1" style='display:none;'>C1</option>
+					<option id="C2" style='display:none;'>C2</option>
+					<option id="C3" style='display:none;'>C3</option>
+					<option id="D1" style='display:none;'>D1</option>
+					<option id="D2" style='display:none;'>D2</option>
+					<option id="D3" style='display:none;'>D3</option>
+					<option id="E1" style='display:none;'>E1</option>
+					<option id="E2" style='display:none;'>E2</option>
+					<option id="E3" style='display:none;'>E3</option>
+				</select>
+			</div>
+
+			<div class="div-radio-demande-submit">
+				<input class="form-cle-api" type="submit" value="Valider">
+			</div>
+			
+
+		</form>
 
 
 </div>
@@ -106,10 +138,11 @@ if (isset($_SESSION['pseudo'])){
 if (isset($_COOKIE["recherche"])){
 	$jasonRecherche = $_COOKIE["recherche"];
 	$recherche = json_decode($jasonRecherche, True);
-	echo "<a id=\"derniere_href\" href='traitement.php?filiere=" . $recherche["filiere"] . "&amp;" ."choix=groupe" . "&amp;" . "groupe=" . $recherche["groupe"] . "&amp;" . "cle=" . $recherche["cle"] ."'>Dernière recherche d'un groupe</a>";
-	//http://nathan-str-prof.alwaysdata.net/traitement.php?filiere=LP-RS&choix=groupe&groupe=D1&cle=U0UWfUlUhxvmOg1HTZZQ
+	echo "<a id=\"derniere_href\" href='traitement.php?filiere=" . $recherche["filiere"] . "&amp;" ."choix=groupe" . "&amp;" . "groupe=" . $recherche["groupe"] ."'>Dernière recherche d'un groupe</a>";
+	echo "<a id=\"derniere_href\" href='traitement.php?choix=filiere" . "&amp;" . "filiere=" . $recherche["filiere"] . "&amp;" . "groupe=" . $recherche["groupe"] ."'>Dernière recherche d'une filière</a>";
+	
 }else{
-	echo"bonjour";
+	echo"Pour avoir accès à sa dernière recherche, il vous faut faire une première recherche. 'Albert Einstein'";
 }
 
 ?>
@@ -120,11 +153,7 @@ if (isset($_COOKIE["recherche"])){
 
 <script src="app.js" meta="utf-8"></script>
 
-<?php
-}else{
-	header("location: ./index.php");
-}
-?>
+
 </body>
 </html>
 
