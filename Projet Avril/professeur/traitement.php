e54f1401968d5d4fc67a95f25f34ec7f594e0aa3
Nathan Sestre <sestre.nathan@orange.fr>
2020-04-30 00:01:09 +0200

    gestion des boutons d'impression et des titres Gestion de redirection si le session n'existe pas

--- Projet Avril/professeur/traitement.php
+++ Projet Avril/professeur/traitement.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 //$jsonText = file_get_contents('http://nathan-str-etudiant.alwaysdata.net/apiEtu.php?choix=filiere&filiere=LPI-RIWS&cle=hbtKnCWKocRDeTDSkijK');
 //$jsonArray = json_decode($jsonText,True);
 //print_r($jsonArray['LPI-RIWS']['E3']['13']['prenom']);//Exemple: $jsonArray['$_POST[filiere]']['$_POST[groupe]']['id(pas sur)']['prenom']
@@ -8,10 +8,14 @@
 
 //}
 
+include 'fonction.php';
+
+verifSession($_SESSION['pseudo']);
+
 function mosaique(){
 	$filiere = $_GET['filiere'];
 	$groupe = $_GET['groupe'];
-	$cle = $_GET['cle'];
+	$cle = "dzsWVDlC1wgTgxC4BquZ";
 
 	if ($_GET['choix'] == "filiere"){
 		if (!empty($cle)){
@@ -20,15 +24,19 @@ function mosaique(){
 			$jsonArray = json_decode($jsonText,True);
 			$cpt = 0;
 
-			
+			echo("<h1 class=en-tete-filiere>Elèves de la filière " . $filiere . ":</h1>");
+
+			echo("<form>");
+				echo("<input type=\"button\" class=\"impression-filiere\" value=\"Imprimer la page\" onClick=\"window.print()\">");
+			echo("</form>");
+
 			for ($i=1; $i <= sizeof($jsonArray["$filiere"]); $i++){ //A mettre plus tard le sizeof du post voulu
 			//<img src=$jsonArray['$_POST[filiere]']['$_POST[groupe]']['id']['image']
 				$cpt += 1;
 				echo("<div class=profil>");
-				echo("<img src=http://nathan-str-etudiant.alwaysdata.net/images/" . $jsonArray["$filiere"][$i]['image'] . "width=170 height=170 style=border-radius:10px; alt=error class=image><br />");
-				echo $jsonArray["$filiere"][$i]['prenom'] . " " . $jsonArray["$filiere"][$i]['nom'] . "<br />";
-				//echo $jsonArray["$filiere"][$i]['mail'] . "<br />";
-				//echo $jsonArray["$filiere"][$i]['numero'] . "<br />";
+				echo("<img src=http://nathan-str-etudiant.alwaysdata.net/images/" . $jsonArray["$filiere"][$i]['image'] . "width=170 height=170 style=border-radius:10px; alt=error class=image onclick=\"clickImage($i)\";><br />");
+				echo "<p>" . $jsonArray["$filiere"][$i]['prenom'] . " " . $jsonArray["$filiere"][$i]['nom'] . "</p><br />";
+				echo "<p class=info id=$i style=\"display: none;\">".$jsonArray["$filiere"][$i]['mail'] . "<br />". $jsonArray["$filiere"][$i]['numero'] . "</p><br />";
 				echo("</div>");
 				
 			}
@@ -52,7 +60,11 @@ function mosaique(){
 				$jsonArray = json_decode($jsonText,True);
 				$cpt = 0;
 
-				echo("<h1>Elèves du groupe " . $groupe . " de la filière " . $filiere . ":</h1>");
+				echo("<h1 class=en-tete-groupe>Elèves du groupe " . $groupe . " de la filière " . $filiere . ":</h1>");
+
+				echo("<form>");
+					echo("<input type=\"button\" class=\"impression-groupe\" value=\"Imprimer la page\" onClick=\"window.print()\">");
+				echo("</form>");
 
 				for ($i=1; $i <= sizeof($jsonArray["$filiere"]["$groupe"]); $i++){
 					$cpt += 1;
@@ -74,43 +86,38 @@ function mosaique(){
 	}
 }
 
-//}elseif(isset($_POST['groupe'])){
-	//$jsonText = file_get_contents('URL pour le groupe');
-		//$jsonArray = json_decode($jsonText,True);
-		//Exemple: $jsonArray['$_POST[filiere]']['$_POST[groupe]']['id(pas sur)']['prenom']
-	//for  sizeof(taille du groupe){
-	//<img src=$jsonArray['$_POST[filiere]']['$_POST[groupe]']['id']['image']
-	//echo $jsonArray['$_POST[filiere]']['$_POST[groupe]']['id(pas sur)']['prenom']
-	//}	
-//}
+
+
+
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-	<title>trombinoscope</title>
+	<title>Administration</title>
 	<link rel="stylesheet" type="text/css" href="styles.css" media="screen">
 	<link rel="stylesheet" type="text/css" href="print.css" media="print">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
 	<header role="header" >	
 			<nav class="menu" role="navigation">
 				<div class="inner">
 					<div class="gauche">
-						<a class="logo" href="index.php">Trombinoscope</a>
+						<a class="logo" href="index.php">Administration</a>
 					</div>
 
 					<div class="droite">
 						
 						<!--<a href="#" class="lien"><i class="fa fa-globe"></i> A propos</a>-->
-						<?php
-							if(!empty($_SESSION['pseudo'])){
-						?>
-								<a href="#" class="lien"><i class='fa fa-book'></i> mosaique</a>
+						<!--<?php
+							//if(!empty($_SESSION['pseudo'])){
+						?>-->
+								<a href="demande.php" class="lien"><i class='fa fa-book'></i> mosaique</a>
 								<a href="deconnexion.php" class="lien"><i class="fa fa-sign-out"></i> Déconnexion</a>
-						<?php
-							}
-						?>
+						<!--<?php
+							//}
+						?>-->
 
 					</div>
 
@@ -122,10 +129,6 @@ function mosaique(){
 			</nav>
 	</header>
 
-
-<form>
-	<input type="button" class="impression" value="Imprimer" onClick="window.print()">
-</form>
 <?php
 mosaique();
 ?>
