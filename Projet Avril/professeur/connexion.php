6592e00ea1cbc61d76635167188fb15f8ba53c75
Nathan Sestre <sestre.nathan@orange.fr>
2020-05-01 23:30:20 +0200

    Changement de la fonction pour la connexion (plus organisée)

--- Projet Avril/professeur/connexion.php
+++ Projet Avril/professeur/connexion.php
@@ -3,6 +3,7 @@ session_start();
 
 include 'fonction.php';
 
-connexion("fichiers/comptes.csv", "demande.php", "index.php");
+//connexion("fichiers/comptes.csv", "demande.php", "index.php");
+connexion1($_POST['login'], $_POST['pwd'], 'fichiers/comptes.csv',"demande.php", "index.php");
 
 ?>
