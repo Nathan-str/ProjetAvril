b709ab1a03f17107d2d9be2e083aa704cf5534a5
Nathan Sestre <sestre.nathan@orange.fr>
2020-05-01 23:32:54 +0200

    ajout des paramètres lors de l'appelle de la fonction inscription1

--- Projet Avril/professeur/inscription.php
+++ Projet Avril/professeur/inscription.php
@@ -2,14 +2,16 @@
 session_start();
 
 include 'fonction.php';
-$continue = verification($_POST['mail'], $_POST['numero'], "fichiers/comptes.csv");
+/*$continue = verification($_POST['mail'], $_POST['numero'], "fichiers/comptes.csv");
 
 if ($continue == true){
 	inscription("fichiers/comptes.csv", "fichiers/id.txt");
 }else{
 	FichierLog("inscription échoué (mail ou numéro déjà utilisé)",$_POST["mail"]);
 	header("location:./index.php?error=5");
-}
+}*/
+
+inscription1($_POST["nom"], $_POST["prenom"], $_POST["mail"], $_POST["numero"], $_POST["mdp"] , $_POST["mdp1"], 'fichiers/comptes.csv', "fichiers/id.txt","demande.php", "index.php");
 
 
 
