b306a8b367ed5f155af56ea7ae343ac2f06e8372
Nathan Sestre <sestre.nathan@orange.fr>
2020-05-01 23:40:07 +0200

    fonction inscription2 pour les filieres, groupes et images

--- Projet Avril/étudiant/inscription.php
+++ Projet Avril/étudiant/inscription.php
@@ -3,9 +3,64 @@
 
 	include 'fonction.php';
 
+	function inscription2($nom, $prenom, $mail, $numero, $mdp , $mdp1, $fichier, $fichierID,$pageRenvoie, $pageErreur){
 
+		verifSession($nom,"redirection.php?error=1");
+		verifSession($prenom, "redirection.php?error=1");
+		verifSession($mail, "redirection.php?error=1");
+		verifSession($numero, "redirection.php?error=1");
+		verifSession($mdp, "redirection.php?error=1");
+		verifSession($mdp1, "redirection.php?error=1");
 
-	$fin = verification($_POST['mail'], $_POST['numero'] , 'fichiers/comptes.csv');
+		$continue = double($mail, "3", $fichier);
+		$suite = double($numero, "4", $fichier);
+		$longueur = longueur($mdp);
+		$longueur2 = longueur($mdp1);
+
+		if($continue == true && $suite == true){
+			if($longueur == true && $longueur2 == true){
+				if($mdp == $mdp1){
+					if($_POST['groupe'] != "Groupe"){
+
+						$car_alea = alea();
+						$secure_mot_de_passe = $car_alea . $mdp;
+
+						$donnes = fopen($fichier, 'a+');
+						$monfichier = fopen($fichierID, 'r+');
+
+						$id = fgets($monfichier); // On lit la première ligne (nombre de pages vues)
+						$id += 1; // On augmente de 1 ce nombre de pages vues
+						fseek($monfichier, 0); // On remet le curseur au début du fichier
+						fputs($monfichier, $id); // On écrit le nouveau nombre de pages vues
+
+						fputs($donnes, $id . ";" . $nom . ";" . $prenom . ";" . $mail . ";" . $numero . ";" . $car_alea . ";" .hash("sha256",$secure_mot_de_passe) . ";" . $_POST['filiere'] . ";" . $_POST['groupe'] . ";" . "profil_defaut.png" ."\n");
+
+						fclose($monfichier);
+						fclose($donnes);
+
+						FichierLog("inscription réussi",$_POST["mail"]);
+						header("location:./$pageErreur?error=0");
+					}else{
+						header("location:./$pageErreur?error=6");
+					}
+
+				}else{
+					FichierLog("inscription échoué (mots de passe différents)",$mail);
+					header("location:./$pageErreur?error=3");
+				}
+				
+			}else{
+				FichierLog("inscription échoué (mot de passe trop petit)",$mail);
+				header("location:./$pageErreur?error=4");
+			}
+		}else{
+			FichierLog("inscription échoué (mail ou numéro déjà utilisé)",$mail);
+			header("location:./$pageErreur?error=5");
+		}
+
+	}
+
+	/*$fin = verification($_POST['mail'], $_POST['numero'] , 'fichiers/comptes.csv');
 
 	if ($fin == true){
 
@@ -46,5 +101,7 @@
 	}elseif ($continue == false){
 		FichierLog("inscription échoué",$_POST["mail"]);
 		header("location:./redirection.php?error=5");
-	}
+	}*/
+
+	inscription2($_POST["nom"], $_POST["prenom"], $_POST["mail"], $_POST["numero"], $_POST["mdp"] , $_POST["mdp1"], 'fichiers/comptes.csv', "fichiers/id.txt","informations.php", "redirection.php");
 ?>
