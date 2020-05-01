7abd629c1c69d7b5b71909dce5c22351c1fd19e1
Nathan Sestre <sestre.nathan@orange.fr>
2020-05-01 23:40:34 +0200

    formualaire JSON filieres

--- Projet Avril/étudiant/redirection.php
+++ Projet Avril/étudiant/redirection.php
@@ -63,7 +63,11 @@ include 'fonction.php';
 					<input class="input" type="password" name="mdp1" minlength="6" placeholder="Confirmation mot de passe" required="required" />
 					<input class="submit" type="submit" value="Valider" />
 					<p id="filiere-p">Filière</p>
-					<input class="radio" type="radio" onclick="selection()" name="filiere" id="L1-MIPI" value="L1-MIPI" checked=""/>L1-MIPI
+					<?php
+						filiereJSON("filiere", "groupe");
+						$jsonText = jsonText();
+					?>
+					<!--<input class="radio" type="radio" onclick="selection()" name="filiere" id="L1-MIPI" value="L1-MIPI" checked=""/>L1-MIPI
 					<input class="radio" type="radio" onclick="selection()" name="filiere" id="L2-MI" value="L2-MI" />L2-MI
 					<input class="radio" type="radio" onclick="selection()" name="filiere" id="L3-I" value="L3-I" />L3-I
 					<input class="radio" type="radio" onclick="selection()" name="filiere" id="LP-RS" value="LP-RS" />LP-RS
@@ -85,7 +89,7 @@ include 'fonction.php';
 						<option id="E1" style='display:none;'>E1</option>
 						<option id="E2" style='display:none;'>E2</option>
 						<option id="E3" style='display:none;'>E3</option>
-					</select>
+					</select>-->
 				</form>
 				<?php
 					errorInscription();
@@ -108,6 +112,31 @@ include 'fonction.php';
 		</div>
 
 	<script src="app.js" meta="utf-8"></script>
+	<script type="text/javascript">
+	function liste_groupe(){
+		affiche_Groupe(<?php  echo($jsonText);?>);
+	}
+
+	function affiche_Groupe(jsonText){
+	    let filiere = document.getElementById("select-filiere").value;
+	    let groupe =document.getElementById("select-groupe");
+
+	    groupe.innerHTML = "<option value=''>Groupe</option>";
+	    //for (let groupe in jsonText["listeFilieres"]){
+	    //    groupe.innerHTML += `<option value='${jsonText["listeFilieres"]["0"]}'>${jsonText["listeFilieres"]["0"]}</option>`;
+	    //}
+			
+		for (var i = 0; i < jsonText["listeFilieres"].length; i++) {
+			if (filiere == jsonText["listeFilieres"][i]["nomFiliere"]) {
+				for (var j = 0; j < jsonText["listeFilieres"][i]["groupes"].length; j++) {
+				
+					groupe.innerHTML += "<option value=" + jsonText["listeFilieres"][i]["groupes"][j] + ">" + jsonText["listeFilieres"][i]["groupes"][j] + "</option>";
+
+				}
+			}
+		}
+	}
+	</script>
 
 		<footer class="le_footer">
 			<div class="contenue">
