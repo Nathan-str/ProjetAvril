41d78ea9fab6a3071cdc558646f69295d7ce0737
Nathan Sestre <sestre.nathan@orange.fr>
2020-05-01 23:40:57 +0200

    Création du JSON pour la filiere

--- Projet Avril/étudiant/test.php
+++ Projet Avril/étudiant/test.php
@@ -20,37 +20,38 @@
 		$jsonArray = array();
 		$jsonTableau = array();
 
-		$jsonArray["A1"] = "A1";
-		$jsonArray["A2"] = "A2";
-		$jsonArray["A3"] = "A3";
-		$jsonTableau["L1-MIPI"] = $jsonArray;
+		//$jsonArray["A1"] = "A1";
+		//$jsonArray["A2"] = "A2";
+		//$jsonArray["A3"] = "A3";
+		$jsonTableau["L1-MIPI"]["groupes"] = ["A1","A2","A3"];
 
 
-		$jsonArray1["B1"] = "B1";
-		$jsonArray1["B2"] = "B2";
-		$jsonArray1["B3"] = "B3";
-		$jsonTableau["L2-MI"] = $jsonArray1;
+		//$jsonArray1["B1"] = "B1";
+		//$jsonArray1["B2"] = "B2";
+		//$jsonArray1["B3"] = "B3";
+		$jsonTableau["L2-MI"]["groupes"] = ["B1","B2","B3"];
 
-		$jsonArray2["C1"] = "C1";
-		$jsonArray2["C2"] = "C2";
-		$jsonArray2["C3"] = "C3";
-		$jsonTableau["L3-I"] = $jsonArray2;
+		//$jsonArray2["C1"] = "C1";
+		//$jsonArray2["C2"] = "C2";
+		//$jsonArray2["C3"] = "C3";
+		$jsonTableau["L3-I"]["groupes"] = ["C1","C2","C3"];
 
-		$jsonArray3["D1"] = "D1";
-		$jsonArray3["D2"] = "D2";
-		$jsonArray3["D3"] = "D3";
-		$jsonTableau["LP-RS"] = $jsonArray3;
+		//$jsonArray3["D1"] = "D1";
+		//$jsonArray3["D2"] = "D2";
+		//$jsonArray3["D3"] = "D3";
+		$jsonTableau["LP-RS"]["groupes"] = ["D1","D2","D3"];
 
-		$jsonArray4["E1"] = "E1";
-		$jsonArray4["E2"] = "E2";
-		$jsonArray4["E3"] = "E3";
-		$jsonTableau["LPI-RIWS"] = $jsonArray4;
-		$jsonBig["All"] = $jsonTableau;
+		//$jsonArray4["groupe"] = "E1";
+		//$jsonArray4["groupe"] = "E2";
+		//$jsonArray4["groupe"] = "E3";
+		$jsonTableau["LPI-RIWS"]["groupes"] = ["E1","E2","E3"];
+		$jsonBig["listeFilieres"] = $jsonTableau;
 
 		$json = $jsonBig;
 		$json = json_encode($json);
 		header('Content-type: application/json');
-		echo($json);
+		$file = fopen("fichiers/filiere.json", "w");
+		fwrite($file, $json);
 	//}
 
 ?>
