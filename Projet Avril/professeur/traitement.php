363ed3c09c5acd42c66ad7f8fa03e5962af242aa
Nathan Sestre <sestre.nathan@orange.fr>
2020-05-01 23:34:52 +0200

    ajout du JS pour le traitement du JSON pour les filieres pour le formulaires adaptatifs ajout des verification des groupes et filieres

--- Projet Avril/professeur/traitement.php
+++ Projet Avril/professeur/traitement.php
@@ -1,93 +1,12 @@
 <?php
 session_start();
-//$jsonText = file_get_contents('http://nathan-str-etudiant.alwaysdata.net/apiEtu.php?choix=filiere&filiere=LPI-RIWS&cle=hbtKnCWKocRDeTDSkijK');
-//$jsonArray = json_decode($jsonText,True);
-//print_r($jsonArray['LPI-RIWS']['E3']['13']['prenom']);//Exemple: $jsonArray['$_POST[filiere]']['$_POST[groupe]']['id(pas sur)']['prenom']
-
-//function genereEleve(){
-
-//}
 
 include 'fonction.php';
 
-verifSession($_SESSION['pseudo']);
+verifSession($_SESSION['pseudo'], "index.php");
 
-function mosaique(){
-	$filiere = $_GET['filiere'];
-	$groupe = $_GET['groupe'];
-	$cle = "dzsWVDlC1wgTgxC4BquZ";
-
-	if ($_GET['choix'] == "filiere"){
-		if (!empty($cle)){
-			$jsonArray = array();
-			$jsonText = file_get_contents('http://nathan-str-etudiant.alwaysdata.net/apiEtu.php?choix=filiere&filiere='.$filiere.'&cle=' .$cle);
-			$jsonArray = json_decode($jsonText,True);
-			$cpt = 0;
-
-			echo("<h1 class=en-tete-filiere>Elèves de la filière " . $filiere . ":</h1>");
-
-			echo("<form>");
-				echo("<input type=\"button\" class=\"impression-filiere\" value=\"Imprimer la page\" onClick=\"window.print()\">");
-			echo("</form>");
-
-			for ($i=1; $i <= sizeof($jsonArray["$filiere"]); $i++){ //A mettre plus tard le sizeof du post voulu
-			//<img src=$jsonArray['$_POST[filiere]']['$_POST[groupe]']['id']['image']
-				$cpt += 1;
-				echo("<div class=profil>");
-				echo("<img src=http://nathan-str-etudiant.alwaysdata.net/images/" . $jsonArray["$filiere"][$i]['image'] . "width=170 height=170 style=border-radius:10px; alt=error class=image onclick=\"clickImage($i)\";><br />");
-				echo "<p>" . $jsonArray["$filiere"][$i]['prenom'] . " " . $jsonArray["$filiere"][$i]['nom'] . "</p><br />";
-				echo "<p class=info id=$i style=\"display: none;\">".$jsonArray["$filiere"][$i]['mail'] . "<br />". $jsonArray["$filiere"][$i]['numero'] . "</p><br />";
-				echo("</div>");
-				
-			}
-		}else{
-			header("location:./demande.php?error=1");
-		}
-		
-	}else{
-
-		if(!empty($groupe)){
-			if (isset($cle)){
-
-				$recherche["filiere"] = $filiere;
-				$recherche["groupe"] = $groupe;
-				$recherche["cle"] = $cle;
-				$jasonRecherche = json_encode($recherche);
-				setcookie("recherche", $jasonRecherche);
-
-				$jsonArray = array();
-				$jsonText = file_get_contents('http://nathan-str-etudiant.alwaysdata.net/apiEtu.php?filiere='. $filiere .'&choix=groupe&groupe='.$groupe .'&cle='.$cle);
-				$jsonArray = json_decode($jsonText,True);
-				$cpt = 0;
-
-				echo("<h1 class=en-tete-groupe>Elèves du groupe " . $groupe . " de la filière " . $filiere . ":</h1>");
-
-				echo("<form>");
-					echo("<input type=\"button\" class=\"impression-groupe\" value=\"Imprimer la page\" onClick=\"window.print()\">");
-				echo("</form>");
-
-				for ($i=1; $i <= sizeof($jsonArray["$filiere"]["$groupe"]); $i++){
-					$cpt += 1;
-					echo("<div class=profil>");
-					echo("<img src=http://nathan-str-etudiant.alwaysdata.net/images/" . $jsonArray["$filiere"]["$groupe"][$i]['image'] . "width=200 height=200 style=border-radius:10px; alt=error class=image onclick=\"clickImage($i)\";><br />");
-					echo "<p>".$jsonArray["$filiere"]["$groupe"][$i]['prenom'] . " " . $jsonArray["$filiere"]["$groupe"][$i]['nom'] . "</p><br />";
-					echo "<p class=info id=$i style=\"display: none;\">".$jsonArray["$filiere"]["$groupe"][$i]['mail'] . "<br />". $jsonArray["$filiere"]["$groupe"][$i]['numero'] . "</p><br />";
-					echo("</div>");
-
-					
-
-				}
-			}else{
-				header("location:./demande.php?error=1");
-			}
-		}else{
-			header("location:./demande.php?error=2");
-		}
-	}
-}
-
-
-
+verifSession($_GET["filiere"], "demande.php");
+verifSession($_GET["groupe"], "demande.php");
 
 ?>
 
@@ -129,10 +48,46 @@ function mosaique(){
 			</nav>
 	</header>
 
+	<div class="formulaire-traitement">
+		<form action="traitement.php" method="get">
+			<?php
+				filiereJSON("filiere", "groupe");
+				$jsonText = jsonText();
+			?>
+			<input type="submit" class="select-submit" value="Valider" />
+		</form>
+	</div>
+
 <?php
-mosaique();
+//mosaique();
+choixMosaïque($_GET["filiere"], $_GET["groupe"], "dzsWVDlC1wgTgxC4BquZ", $_GET["choix"]);
 ?>
 
 <script src="app.js" meta="utf-8"></script>
+<script type="text/javascript">
+	function liste_groupe(){
+		affiche_Groupe(<?php  echo($jsonText);?>);
+	}
+
+	function affiche_Groupe(jsonText){
+	    let filiere = document.getElementById("select-filiere").value;
+	    let groupe =document.getElementById("select-groupe");
+
+	    groupe.innerHTML = "<option value=''>Groupe</option>";
+	    //for (let groupe in jsonText["listeFilieres"]){
+	    //    groupe.innerHTML += `<option value='${jsonText["listeFilieres"]["0"]}'>${jsonText["listeFilieres"]["0"]}</option>`;
+	    //}
+			
+		for (var i = 0; i < jsonText["listeFilieres"].length; i++) {
+			if (filiere == jsonText["listeFilieres"][i]["nomFiliere"]) {
+				for (var j = 0; j < jsonText["listeFilieres"][i]["groupes"].length; j++) {
+				
+					groupe.innerHTML += "<option value=" + jsonText["listeFilieres"][i]["groupes"][j] + ">" + jsonText["listeFilieres"][i]["groupes"][j] + "</option>";
+
+				}
+			}
+		}
+	}
+</script>
 </body>
 </html>
